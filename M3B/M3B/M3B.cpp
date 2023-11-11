6764ad9f8ab8905c80bfa69fa88c6c47776e3b14
MSIvanov21 <MSIvanov21@codingburgas.bg>
2023-11-11 20:51:35 +0200

    Add Replication text in game. Create medium difficulty (Replication Game)

--- M3B/M3B/M3B.cpp
+++ M3B/M3B/M3B.cpp
@@ -34,6 +34,21 @@ void info()
     }
 }
 
+void replication()
+{
+    fstream newfile;
+
+    newfile.open("..\\..\\textFiles\\replication.txt", ios::in);
+    if (newfile.is_open()) {
+        string tp;
+        while (getline(newfile, tp)) {
+            cout << setw(170) << tp << endl;
+        }
+        newfile.close();
+    }
+}
+
+
 void firstPlace()
 {
     fstream newfile;
@@ -152,17 +167,19 @@ int main()
     } break;
     case 2:
     {
+        system("cls");
+        replication();
         switch (tempDiffChoose) // switching the difficulty
         {
         case 1: // Game : Replication | Difficulty : Easy
         {
             int points = 0;
-            char answer[3];
-            char arr[3] = { 'G' , 'A', 'C' };
+            char* answer = new char[3];
+            char basesEasy[3] = { 'G', 'A', 'C' };
             cout << "Your job is to pair perfectly the RNA to the DNA Matrix." << endl;
             for (int i = 0; i < 3; i++)
             {
-                cout << arr[i] << " ";
+                cout << basesEasy[i] << " ";
             }
             cout << " | Write the  Nucleotide Bases properly." << endl;
             for (int i = 0; i < 3; i++)
@@ -172,29 +189,69 @@ int main()
             cout << endl;
             for (int i = 0; i < 3; i++)
             {
-                if (answer[i] == 'C' && arr[i] == 'G')
-                { 
-                    points += 2;
-                }
-                else if (answer[i] == 'U' && arr[i] == 'A')
+                if (answer[i] == 'C' && basesEasy[i] == 'G')
                 {
-                    points += 2;
+                    points++;
                 }
-                else if (answer[i] =='G' && arr[i] == 'C')
+                else if (answer[i] == 'U' && basesEasy[i] == 'A')
                 {
-                    points += 2;
+                    points++;
+                }
+                else if (answer[i] == 'G' && basesEasy[i] == 'C')
+                {
+                    points++;
                 }
             }
-            if (points == 2)   // Rewards
+            if (points == 1)   // Rewards
                 thirdPlace();
-
-            else if (points == 4)
+            else if (points == 2)
                 secondPlace();
-
-            else if (points == 6)
+            else if (points == 3)
                 firstPlace();
 
-        }
+            delete[] answer; 
+            
+        }break;
+        case 2:  // Game : Replication | Difficulty : Medium
+            {
+            int points = 0;
+            char* answer = new char[5]; 
+            char basesMedium[5] = { 'A', 'C', 'A', 'G', 'C' };
+            cout << "Your job is to pair perfectly the RNA to the DNA Matrix." << endl;
+
+
+            for (int i = 0; i < 5; i++)
+                cout << basesMedium[i] << " "; // Outputs the DNA Matrix.
+
+            cout << " | Write the  Nucleotide Bases properly." << endl;
+
+            for (int i = 0; i < 5; i++)
+                cin >> answer[i];   // Inputs the RNA Matrix as an answer.
+
+            cout << endl;
+            for (int i = 0; i < 5; i++) // For loop for checking if the DNA and RNA Matches also adding points if it's correct.
+            {
+                if (answer[i] == 'C' && basesMedium[i] == 'G')
+                    points++;
+
+                else if (answer[i] == 'U' && basesMedium[i] == 'A')
+                    points++;
+
+                else if (answer[i] == 'G' && basesMedium[i] == 'C')
+                    points++;
+            }
+            if (points == 1 || points == 2)   // Outputs the reward.
+                thirdPlace();
+
+            else if (points == 3 || points == 4)
+                secondPlace();
+
+            else if (points == 5)
+                firstPlace();
+
+            delete[] answer;
+            
+            }
         }
     } break;
     case 3:
