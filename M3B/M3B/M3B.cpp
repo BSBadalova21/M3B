3612e0f0df6b680f5a126c88e43fd1be30f971ea
MSIvanov21 <MSIvanov21@codingburgas.bg>
2023-11-12 12:15:28 +0200

    Move Replication game and Information menu in separate functions.

--- M3B/M3B/M3B.cpp
+++ M3B/M3B/M3B.cpp
@@ -6,15 +6,21 @@
 
 using namespace std;
 
-void welcome()
+int diffChoose[50];
+int arrChoose[50];
+int tempDiffChoose;
+int tempArrChoose;
+
+
+void nameOfTheGame()
 {
     fstream newfile;
 
-    newfile.open("..\\..\\textFiles\\welcome.txt", ios::in); //open a file to perform read operation using file object
+    newfile.open("..\\..\\textFiles\\nameOfTheGame.txt", ios::in); //open a file to perform read operation using file object
     if (newfile.is_open()) { //checking whether the file is open
         string tp;
         while (getline(newfile, tp)) { //read data from file object and put it into string.
-            cout << setw(170) << tp << endl; //print the data of the string
+            cout << setw(135) << tp << endl; //print the data of the string
         }
         newfile.close(); //close the file object.
     }
@@ -28,7 +34,7 @@ void info()
     if (newfile.is_open()) { 
         string tp;
         while (getline(newfile, tp)) { 
-            cout << setw(170) << tp << endl; 
+            cout << setw(135) << tp << endl; 
         }
         newfile.close();
     }
@@ -42,10 +48,11 @@ void replication()
     if (newfile.is_open()) {
         string tp;
         while (getline(newfile, tp)) {
-            cout << setw(170) << tp << endl;
+            cout << setw(135) << tp << endl;
         }
         newfile.close();
     }
+    
 }
 void game3()
 {
@@ -55,7 +62,7 @@ void game3()
     if (newfile.is_open()) {
         string tp;
         while (getline(newfile, tp)) {
-            cout << setw(130) << tp << endl;
+            cout << setw(135) << tp << endl;
         }
         newfile.close();
     }
@@ -114,6 +121,114 @@ void dna()
         }
         newfile.close();
     }
+}
+void secondGame()
+{
+    cout << "1. Easy" << endl;
+    cout << "2. Medium" << endl;
+    cout << "3. Hard" << endl;
+    cout << "Choose your difficulty: ";
+    for (int j = 0; j < 50; j++)
+    {
+        cin >> diffChoose[j];
+        if (diffChoose[j] <= 0) {
+            cout << "Wrong input.Try again." << endl;
+        }
+        else if (diffChoose[j] > 3) {
+            cout << "Wrong input.Try again." << endl;
+        }
+        else {
+            tempDiffChoose = diffChoose[j];
+            break;
+
+        }
+    }
+     
+    switch ( tempDiffChoose ) // switching the difficulty
+    {
+        case 1: // Game : Replication | Difficulty : Easy
+        {
+        int points = 0;
+        char* answer = new char[3];
+        char basesEasy[3] = { 'G', 'A', 'C' };
+        cout << "Your job is to pair perfectly the RNA to the DNA Matrix." << endl;
+        for (int i = 0; i < 3; i++)
+        {
+            cout << basesEasy[i] << " ";
+        }
+        cout << " | Write the  Nucleotide Bases properly." << endl;
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> answer[i];
+        }
+        cout << endl;
+        for (int i = 0; i < 3; i++)
+        {
+            if (answer[i] == 'C' && basesEasy[i] == 'G')
+            {
+                points++;
+            }
+            else if (answer[i] == 'U' && basesEasy[i] == 'A')
+            {
+                points++;
+            }
+            else if (answer[i] == 'G' && basesEasy[i] == 'C')
+            {
+                points++;
+            }
+        }
+        if (points == 1)   // Rewards
+            thirdPlace();
+        else if (points == 2)
+            secondPlace();
+        else if (points == 3)
+            firstPlace();
+
+        delete[] answer;
+
+        }break;
+        case 2:  // Game : Replication | Difficulty : Medium
+        {
+        int points = 0;
+        char* answer = new char[5];
+        char basesMedium[5] = { 'A', 'C', 'A', 'G', 'C' };
+        cout << "Your job is to pair perfectly the RNA to the DNA Matrix." << endl;
+
+
+        for (int i = 0; i < 5; i++)
+            cout << basesMedium[i] << " "; // Outputs the DNA Matrix.
+
+        cout << " | Write the  Nucleotide Bases properly." << endl;
+
+        for (int i = 0; i < 5; i++)
+            cin >> answer[i];   // Inputs the RNA Matrix as an answer.
+
+        cout << endl;
+        for (int i = 0; i < 5; i++) // For loop for checking if the DNA and RNA Matches also adding points if it's correct.
+        {
+            if (answer[i] == 'C' && basesMedium[i] == 'G')
+                points++;
+
+            else if (answer[i] == 'U' && basesMedium[i] == 'A')
+                points++;
+
+            else if (answer[i] == 'G' && basesMedium[i] == 'C')
+                points++;
+        }
+        if (points == 1 || points == 2)   // Outputs the reward.
+            thirdPlace();
+
+        else if (points == 3 || points == 4)
+            secondPlace();
+
+        else if (points == 5)
+            firstPlace();
+
+        delete[] answer;
+
+        }
+    }
+    
 }
 void thirdGame()
 {
@@ -223,14 +338,32 @@ void thirdGame()
         cout << endl << "YOU HAVE WON THE GAME!" << endl << "HERE'S YOUR MEDAL!" << endl;
         firstPlace();
     }
+
+
+    
 }
+
+void informationText() {
+    cout << endl;
+    cout << endl;
+    cout << "DNA Info:" << endl;
+    cout << "A - T (Adenine - Thymine)" << endl;
+    cout << "G - C (Guanine - Cytosine)" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "RNA Info:" << endl;
+    cout << "A - U (Adenine - Uracil)" << endl;
+    cout << "G - C (Same as DNA)" << endl;
+    cout << endl;
+    cout << endl;
+}
+
+
+
 int main()
 {
-    welcome();
-    int diffChoose[50];
-    int arrChoose[50];
-    int tempDiffChoose;
-    int tempArrChoose;
+    nameOfTheGame();
+
     cout << endl;
     cout << endl;
     cout << "1. Information" << endl;
@@ -255,25 +388,7 @@ int main()
                 break;
             }
             tempArrChoose = arrChoose[i];
-            cout << "1. Easy" << endl;
-            cout << "2. Medium" << endl;
-            cout << "3. Hard" << endl;
-            cout << "Choose your difficulty: ";
-            for (int j = 0; j < 50; j++)
-            {
-                cin >> diffChoose[j];
-                if (diffChoose[j] <= 0){
-                    cout << "Wrong input. Please input number between 1 and 3.";
-                }
-                else if (diffChoose[j] > 3) {
-                    cout << "Wrong input. Please input number between 1 and 3.";
-                }
-                else {
-                    tempDiffChoose = diffChoose[j];
-                    break;
-
-                }
-            }
+            
             break;
         }
     }
@@ -288,112 +403,18 @@ int main()
         system("cls");
         info();
         dna();
-        cout << endl;
-        cout << endl;
-        cout << "DNA Info:" << endl;
-        cout << "A - T (Adenine - Thymine)" << endl;
-        cout << "G - C (Guanine - Cytosine)" << endl;
-        cout << endl;
-        cout << endl;
-        cout << "RNA Info:" << endl;
-        cout << "A - U (Adenine - Uracil)" << endl;
-        cout << "G - C (Same as DNA)" << endl;
-        cout << endl;
-        cout << endl;
+        informationText();
         dna();
     } break;
     case 2:
     {
         system("cls");
         replication();
-        switch (tempDiffChoose) // switching the difficulty
-        {
-        case 1: // Game : Replication | Difficulty : Easy
-        {
-            int points = 0;
-            char* answer = new char[3];
-            char basesEasy[3] = { 'G', 'A', 'C' };
-            cout << "Your job is to pair perfectly the RNA to the DNA Matrix." << endl;
-            for (int i = 0; i < 3; i++)
-            {
-                cout << basesEasy[i] << " ";
-            }
-            cout << " | Write the  Nucleotide Bases properly." << endl;
-            for (int i = 0; i < 3; i++)
-            {
-                cin >> answer[i];
-            }
-            cout << endl;
-            for (int i = 0; i < 3; i++)
-            {
-                if (answer[i] == 'C' && basesEasy[i] == 'G')
-                {
-                    points++;
-                }
-                else if (answer[i] == 'U' && basesEasy[i] == 'A')
-                {
-                    points++;
-                }
-                else if (answer[i] == 'G' && basesEasy[i] == 'C')
-                {
-                    points++;
-                }
-            }
-            if (points == 1)   // Rewards
-                thirdPlace();
-            else if (points == 2)
-                secondPlace();
-            else if (points == 3)
-                firstPlace();
-
-            delete[] answer; 
-            
-        }break;
-        case 2:  // Game : Replication | Difficulty : Medium
-            {
-            int points = 0;
-            char* answer = new char[5]; 
-            char basesMedium[5] = { 'A', 'C', 'A', 'G', 'C' };
-            cout << "Your job is to pair perfectly the RNA to the DNA Matrix." << endl;
-
-
-            for (int i = 0; i < 5; i++)
-                cout << basesMedium[i] << " "; // Outputs the DNA Matrix.
-
-            cout << " | Write the  Nucleotide Bases properly." << endl;
-
-            for (int i = 0; i < 5; i++)
-                cin >> answer[i];   // Inputs the RNA Matrix as an answer.
-
-            cout << endl;
-            for (int i = 0; i < 5; i++) // For loop for checking if the DNA and RNA Matches also adding points if it's correct.
-            {
-                if (answer[i] == 'C' && basesMedium[i] == 'G')
-                    points++;
-
-                else if (answer[i] == 'U' && basesMedium[i] == 'A')
-                    points++;
-
-                else if (answer[i] == 'G' && basesMedium[i] == 'C')
-                    points++;
-            }
-            if (points == 1 || points == 2)   // Outputs the reward.
-                thirdPlace();
-
-            else if (points == 3 || points == 4)
-                secondPlace();
-
-            else if (points == 5)
-                firstPlace();
-
-            delete[] answer;
-            
-            }
-        }
+        secondGame();
     } break;
     case 3:
     {
         thirdGame();
-    }
+    } break;
     }
 }
